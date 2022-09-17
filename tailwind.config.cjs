9957bc123066e38740b4c452ae7d4ad701969435
icesplendent <icesplendent@gmail.com>
2022-09-17 14:32:23 +0800

    core: initial setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	theme: {
+		extend: {},
+		screens: {
+			xss: "320px",
+			xs: "375px",
+			sm: "480px",
+			smd: "510px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+			mxl: "1980px",
+			xxl: "2560px",
+		},
+	},
+	plugins: [],
+};
